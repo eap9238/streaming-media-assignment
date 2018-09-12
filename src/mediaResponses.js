18e84c3d8494e5e473091664a757402cb003d520
DALEK-PC\Eleanor <eap9238@g.rit.edu>
2018-09-11 21:54:40 -0400

    added client3, client2 not working yet

--- src/mediaResponses.js
+++ src/mediaResponses.js
@@ -2,55 +2,57 @@ const fs = require('fs');
 const path = require('path');
 
 const loadFile = (request, response, fileName, fileType) => {
-  const file = path.resolve(__dirname, fileName);
+    const file = path.resolve(__dirname, fileName);
 
-  fs.stat(file, (err, stats) => {
-    if (err) {
-      if (err.code === 'ENOENT') {
-        response.writeHead(404);
-      }
+    fs.stat(file, (err, stats) => {
+        if (err) {
+            if (err.code === 'ENOENT') {
+                response.writeHead(404);
+            }
 
-      return response.end(err);
-    }
+            return response.end(err);
+        }
 
-    let { range } = request.headers;
+        let { range } = request.headers;
 
-    if (!range) {
-      range = 'bytes=0-';
-    }
+        if (!range) {
+            range = 'bytes=0-';
+        }
 
-    const positions = range.replace(/bytes=/, '').split('-');
+        const positions = range.replace(/bytes=/, '').split('-');
 
-    let start = parseInt(positions[0], 10);
+        let start = parseInt(positions[0], 10);
 
-    const total = stats.size;
-    const end = positions[1] ? parseInt(positions[1], 10) : total - 1;
+        const total = stats.size;
+        const end = positions[1] ? parseInt(positions[1], 10) : total - 1;
 
-    if (start > end) {
-      start = end - 1;
-    }
+        if (start > end) {
+            start = end - 1;
+        }
 
-    const chunksize = (end - start) + 1;
+        const chunksize = (end - start) + 1;
 
-    response.writeHead(206, {
-      'Content-Range': `bytes ${start}-${end}/${total}`,
-      'Accept-Ranges': 'bytes',
-      'Content-Length': chunksize,
-      'Content-Type': fileType,
+        response.writeHead(206, {
+            'Content-Range': `bytes ${start}-${end}/${total}`,
+            'Accept-Ranges': 'bytes',
+            'Content-Length': chunksize,
+            'Content-Type': fileType,
+        });
+
+        console.log(fileType);
+
+        const stream = fs.createReadStream(file, { start, end });
+
+        stream.on('open', () => {
+            stream.pipe(response);
+        });
+
+        stream.on('error', (streamErr) => {
+            response.end(streamErr);
+        });
+
+        return stream;
     });
-
-    const stream = fs.createReadStream(file, { start, end });
-
-    stream.on('open', () => {
-      stream.pipe(response);
-    });
-
-    stream.on('error', (streamErr) => {
-      response.end(streamErr);
-    });
-
-    return stream;
-  });
 };
 
 const getParty = (request, response) => {
